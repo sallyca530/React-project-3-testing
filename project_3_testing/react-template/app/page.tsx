ef1f757b480181b5fe857c1b1e846973036a9673
sallyca530 <sally.ca530@gmail.com>
2024-01-30 18:52:56 -0800

    react and flask update

--- project_3_testing/react-template/app/page.tsx
+++ project_3_testing/react-template/app/page.tsx
@@ -3,22 +3,15 @@ import Image from "next/image";
 import styles from './page.module.scss'
 import axios from 'axios'
 import {useState, useEffect} from 'react'
-// import React, { useEffect, useState } from "react";
-
-
-// const API = axios.create({ baseURL: 'http://127.0.0.1:5000'})
-// export const fetchData = () => API.get(`/check_mongodb`);
-// export default API;
-
-
 
 function Home()  {
-  // const API = axios.create({baseURL: "http://127.0.0.1:5000/check_mongodb"})
-  // const url = 'http://127.0.0.1:5000/check_mongodb';
+
   const [data, setData] = useState([{
     id: null,
     name: null,
-    type: null
+    type: null,
+    gender: null,
+    state: null
   }]);
 
   useEffect(() => {
@@ -30,15 +23,14 @@ function Home()  {
         console.log(error);
       });
   }, []);
-  // useEffect(() => { axios.get(url)
-  //   .then(response => console.log(response.data));
+
   console.log(data) 
   return (
     <div className={styles.Container}>
-      Hello World
       <ul>
       {data.map(data => (
-          <li key={data.id}>{data.name},{data.type} </li>
+          <li key={data.id}>{data.name},{data.type},
+          {data.gender},{data.state} </li>
       ))}
       </ul>
       
@@ -47,32 +39,3 @@ function Home()  {
   )
 }
 export default Home;
-// // src/components/DataComponent.js
-// import React, { useState, useEffect } from 'react';
-// // import axios from 'axios';
-
-// const DataComponent = () => {
-//   const [data, setData] = useState(null);
-
-//   useEffect(() => {
-//     const fetchData = async () => {
-//       try {
-//         const response = await axios.get('http://127.0.0.1:5000/check_mongodb');
-//         setData(response.data);
-//       } catch (error) {
-//         console.error('Error fetching data:', error);
-//       }
-//     };
-
-//     fetchData();
-//   }, []);
-
-//   return (
-//     <div>
-//       <h1>Data from Flask API:</h1>
-//       {data && <p>{data.message}</p>}
-//     </div>
-//   );
-// };
-
-// export default DataComponent;
